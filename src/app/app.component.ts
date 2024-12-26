3bc53700d1f906cdd2f998711784af75506e66de
toni Filho <oficinadosdrones@gmail.com>
2024-12-26 21:19:52 +0000

    resposive usando @HostListener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { MainComponent } from "./main/main.component";
+import { Component, HostListener, signal } from '@angular/core';
+import { MainComponent } from './main/main.component';
 import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ RightSidebarComponent, MainComponent],
+  imports: [RightSidebarComponent, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   isRightSidebarCollapsed = signal<boolean>(false);
-
-  changeIsleftSidebarCollapse(isRightSidebarCollapsed : boolean) {
-   this.isRightSidebarCollapsed.set(isRightSidebarCollapsed);
+  /**pegaremos o tamanho da ela */
+  screnWidth = signal<number>(window.innerWidth);
+  /**HostListener ficará observando o tamanho da ela, em caso de small devices faremos a ela responsiva */
+  @HostListener('window:resize')
+  onResize() {
+    /**Atualizando a tela com valor atual */
+    this.screnWidth.set(window.innerWidth);
+    if (this.screnWidth() < 768) {
+      /**Com isto eu fechi«o sidebar em caso de devices portateis */
+      this.isRightSidebarCollapsed.set(true);
+      
+    } 
   }
-  
+
+  changeIsleftSidebarCollapse(isRightSidebarCollapsed: boolean) {
+    this.isRightSidebarCollapsed.set(isRightSidebarCollapsed);
+  }
 }
